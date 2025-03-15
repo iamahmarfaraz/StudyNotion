c3d7ad9f75e714734c04286cdad4e2ef80f3db64
Ahmar Faraz <138380197+iamahmarfaraz@users.noreply.github.com>
2025-03-15 19:02:39 +0530

    Update resetPassword.js

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -31,7 +31,7 @@ exports.resetPasswordToken = async (req, res) => {
     );
 
     // create url
-    const url = `http://localhost:3000/update-password/${token}`;
+    const url = `https://studynotion-frontend-coral.vercel.app/update-password/${token}`;
 
     // send mail conatining the URL
     await mailSender(email,"Password Reset",`Howdy !! You
